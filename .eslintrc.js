fa17e29a4c7421e0be5f302c41932bb16bea03c3
Lakshamana <guitrompa1@gmail.com>
2019-12-30 20:25:34 -0300

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": ["react-app", "prettier"],
+  "plugins": ["react", "prettier"],
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "prettier/prettier": [
+        "error",
+        {
+          "printWidth": 80,
+          "trailingComma": "none",
+          "semi": false,
+          "jsxSingleQuote": true,
+          "singleQuote": true
+        }
+    ]
+  }
+}
