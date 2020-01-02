b2d9e73669d3e906bb6e7187be9ff59695f3d0bc
Lakshamana <guitrompa1@gmail.com>
2020-01-02 10:29:46 -0300

    add switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'materialize-css/dist/css/materialize.min.css'
 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
+import Post from './components/Post'
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
         <Navbar />
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:id' component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   )
